acda80651729e9ea11bdacdade9fc580d3c2e486
Eric Oleson <moleso587@west-mec.org>
2021-03-12 22:15:33 -0700

    finished animation, added styled text, videos, and a footer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function(){
     var viewRemaining = $('nav').css('height');
     $('#viewportContent').css('height', 'calc(100vh - ' + viewRemaining + ')');
-    $('#logoDIV').css('height', 'calc((' + $('#logoDIV').css('width') + ' * 545) / 590');
-    $('#logoBack').css({'height': '' + $('#logoFront').css('height'), 'width': '' + $('#logoFront').css('width'), 'top': '-' + $('#logoFront').css('height')});
+    $('#logoFront').css('height', 'calc((' + $('#logoFront').css('width') + ' * 545) / 590');
+    $('#logoBack').css({'height': '' + $('#logoFront').css('height'), 'width': '' + $('#logoFront').css('width')});
 });
