725be5297b03c54d8cea797c3dc9e3f18a046075
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2021-03-12 18:22:23 -0700

    created image and copy for css animation (unfinished), and script was used for sizing

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(function(){
     var viewRemaining = $('nav').css('height');
     $('#viewportContent').css('height', 'calc(100vh - ' + viewRemaining + ')');
+    $('#logoDIV').css('height', 'calc((' + $('#logoDIV').css('width') + ' * 545) / 590');
+    $('#logoBack').css({'height': '' + $('#logoFront').css('height'), 'width': '' + $('#logoFront').css('width'), 'top': '-' + $('#logoFront').css('height')});
 });
